44c929f4f53997b99727ac5fc5a44792ce3915fc
Djalma <djalmaloliveira@gmail.com>
2019-04-12 00:09:04 -0300

    Add files via upload

---
+++ CalculoMediaInfinita.cpp
@@ -0,0 +1,28 @@
+/*
+/*	Programa para calcular a media entre varios valores. O total de termos é indefinido,
+ao final será apresentado a media de todos os dados inseridos.
+*/
+#include <stdio.h>
+
+main(){
+
+	float num = 1, res;
+	int count = 0;
+
+	printf("\nPrograma para calcular a media de varios numeros");
+	printf("\n\nInforme o valor 0(zero) para realizar a media.");
+
+	while(num!=0)
+	{
+		num = 0;
+		count++;  
+		printf("\n\nInforme o proximo numero(%d): ",count);
+		scanf("%f",&num);
+		res = res + num;		
+	}	
+	printf("\n\nA media dos %d numero eh: %.2f",(count - 1),res/(count - 1));
+	printf("\n\n");	
+
+	return 0;
+}
+
