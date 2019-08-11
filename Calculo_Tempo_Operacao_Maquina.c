4d454866cc860a55321968e74db64b9b8f13518d
Djalma <djalmaloliveira@gmail.com>
2019-08-11 17:28:25 -0300

    Programa da lista de exercicios
    Calculo de horas de trabalho

---
+++ Calculo_Tempo_Operacao_Maquina.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<libio.h>
+
+struct Tempos{
+    int iH;
+    int iM;
+    int iS;
+}Tempo;
+
+void CalculoDuracao(int iTimeT){
+
+    int iHora; float fMinu, fSeg;
+
+    iHora = iTimeT/3600;
+    Tempo.iH = iHora;
+    Tempo.iM = (iTimeT%3600)/60;
+    Tempo.iS = (iTimeT%3600)%60;
+}
+
+main(){
+
+    int iTempo;
+
+    printf("Calculo de horas trabalhadas a partir do tempo em segundos");
+    printf("Favor informar quantos seguindos: ");
+    scanf("%d",&iTempo);
+
+    CalculoDuracao(iTempo);
+
+
+    printf("\n\nO tempo total em horas eh : %d:%d:%d",Tempo.iH,Tempo.iM,Tempo.iS);
+
+
+}
